1a2451ad8ec69c0efbd8e66cb7d8554386f76545
nadeem shakeeb <snadeemshakeeb@gmail.com>
2022-01-20 12:15:14 +0530

    "First Commit"

---
+++ access_mod/src/default_2/A2.java
@@ -0,0 +1,14 @@
+package default_2;
+
+import default1.*; 
+public class A2 {
+	
+	    public static void main(String args[]) 
+	    { 
+	         A1 obj = new  A(); 
+	  
+	        obj.display(); 
+	    }
+
+		
+}
---
+++ access_mod/src/public_2/D2.java
@@ -0,0 +1,12 @@
+package public_2;
+import public_.*;
+public class D2 {
+
+		public static void main(String args[])
+		{
+			D1 obj = new D1();
+			obj.display();
+		}
+	}
+
+
